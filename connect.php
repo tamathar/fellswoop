55f06147850ef94b39c9821d477da66bf31827ad
Tamathar Al'Thor <tamathar2009@gmail.com>
2012-05-08 22:11:35 -0500

    maybe

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
   //Connecting to the server
- $con = mysql_connect("localhost","root","password");    // machine name, username and password
+ $con = mysql_connect("mysql3.000webhost.com","a1704801_tam","Erodae5") or die(mysql_error());    // machine name, username and password
  
   if (!$con)
   {
@@ -10,6 +10,6 @@
   
   //select database
   
-		mysql_select_db("othello",  $con);
+		mysql_select_db("a1704801_tds09b",  $con);
 		
 ?>
